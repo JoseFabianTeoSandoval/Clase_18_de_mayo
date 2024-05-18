cae4c90e71b06248bb832fba51f6241f4198c3e5
JoseFabianTeoSandoval <jteos1@miumg.edu.gt>
2024-05-18 10:06:28 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using Clase_18_de_mayo.data.dataAcces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase_18_de_mayo
+{
+    public partial class Form1 : Form
+    {
+        private PDB personaje;
+        public Form1()
+        {
+            InitializeComponent();
+            personaje = new PDB();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (personaje.Provarconeccion())
+            {
+                MessageBox.Show("si se pudo");
+            }else
+            {
+                MessageBox.Show("nel paste");
+            }
+        }
+    }
+}
---
+++ data/dataAcces/PDB.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_18_de_mayo.data.dataAcces
+{
+    internal class PDB
+    {
+        private string connectionString = "Server =localhost;Database=mi_base_datos;Uid=root;Pwd=Shyro#23112004";
+        public bool Provarconeccion()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    return true;
+                } catch (Exception ) 
+                { 
+                    return false; 
+                }
+            }
+        }
+    }
+}
